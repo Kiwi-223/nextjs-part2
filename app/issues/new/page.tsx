31f1149d5979b895c1e268bde574bd36051d48f3
Kiwi-223 <149635096+Kiwi-223@users.noreply.github.com>
2024-06-28 20:06:21 -0600

    add a markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import { Button, TextField } from "@radix-ui/themes";
-import { TextArea } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 import React from "react";
 
 const NewIssuePage = () => {
   return (
     <div className="max-w-xl space-y-3">
       <TextField.Root placeholder="Title" />
-      <TextArea placeholder="Decription" />
+      <SimpleMDE placeholder="Decription" />
       <Button>Submit New Issue</Button>
     </div>
   );
